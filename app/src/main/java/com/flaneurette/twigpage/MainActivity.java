630952e4a22d45b87723355ab9eec1eb5f0199e3
Flaneurette <info@flaneurette.nl>
2023-01-04 11:21:38 +0100

    updates

--- app/src/main/java/com/flaneurette/twigpage/MainActivity.java
+++ app/src/main/java/com/flaneurette/twigpage/MainActivity.java
@@ -1,5 +1,9 @@
 package com.flaneurette.twigpage;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -15,6 +19,7 @@ import com.flaneurette.twigpage.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    public static final String CHANNEL_ID = "Chat";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +30,30 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_chat, R.id.navigation_settings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-
+        this.createNotificationChannel();
     }
 
+    public void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager notificationManager = this.getSystemService(NotificationManager.class);
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            channel.enableLights(true);
+            channel.setLightColor(Color.BLUE);
+            channel.enableVibration(true);
+            channel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 }
+
--- app/src/main/java/com/flaneurette/twigpage/ui/chat/Message.java
+++ app/src/main/java/com/flaneurette/twigpage/ui/chat/Message.java
@@ -1,5 +1,8 @@
 package com.flaneurette.twigpage.ui.chat;
 
+import static com.flaneurette.twigpage.MainActivity.CHANNEL_ID;
+import static java.lang.Boolean.TRUE;
+
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -7,15 +10,17 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Build;
 import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+
+import com.flaneurette.twigpage.MainActivity;
 import com.flaneurette.twigpage.R;
 
 import java.security.SecureRandom;
 
 public class Message {
-
-    public static final String CHANNEL_ID = "Chat";
 
     Context context;
 
@@ -26,36 +31,22 @@ public class Message {
     @JavascriptInterface
     public void MessageShow(String message) {
 
-        NotificationManager notificationManager = null;
+        NotificationManager notificationManager = context.getSystemService(android.app.NotificationManager.class);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            NotificationManager NotificationManager = context.getSystemService(android.app.NotificationManager.class);
+        if(notificationManager.areNotificationsEnabled() == TRUE) {
+
+            Notification notify = new NotificationCompat.Builder(context, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.notification_icon)
+                    .setContentTitle("You received a new chat")
+                    .setContentText(message)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .build();
+
+            SecureRandom random = new SecureRandom();
+            byte bytes[] = new byte[20];
+            random.nextBytes(bytes);
+            int randomInt = random.nextInt(20);
+            notificationManager.notify(randomInt, notify);
         }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "Chat";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(message);
-            channel.enableLights(true);
-            channel.setLightColor(Color.BLUE);
-            channel.enableVibration(true);
-            channel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
-            notificationManager.createNotificationChannel(channel);
-        }
-
-        Notification notify = new NotificationCompat.Builder(context.getApplicationContext(), CHANNEL_ID)
-                .setSmallIcon(R.drawable.notification_icon)
-                .setContentTitle("You received a new chat")
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .build();
-
-        SecureRandom random = new SecureRandom();
-        byte bytes[] = new byte[20];
-        random.nextBytes(bytes);
-        int randomInt = random.nextInt(20);
-        // assert notificationManager != null;
-        notificationManager.notify(randomInt, notify);
     }
 }
